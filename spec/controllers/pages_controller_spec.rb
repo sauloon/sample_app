952a018b4c09bda74b04e5b71a0a531df6012d98
sauloon <sauloon@gmail.com>
2011-06-15 07:01:38 +0000

    Added Help page tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -51,4 +51,21 @@ describe PagesController do
     end
   end
 
+  ############################################
+  # Help Page Tests
+  ############################################
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector( "title",
+                                     :content => "Ruby on Rails Tutorial Sample App | Help" )
+    end
+  end
+
+
 end
